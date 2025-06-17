a2c213926801085994f61ec35a7245139a2653ce
Agung Mulia <snubsgungs@gmail.com>
2025-06-17 17:00:21 +0700

    chore: change export value

--- Grab/services/book_ride.py
+++ Grab/services/book_ride.py
@@ -4,7 +4,7 @@ import uiautomator2 as u2
 import time
 from services.general import check_login_status, clear_unexpected_popups
 
-def main():
+def book_ride():
     d = u2.connect()
     d.app_start("com.grab.taxibooking")
     time.sleep(5)
@@ -20,4 +20,10 @@ def main():
     print("📲 Proceeding to book ride...")
 
 if __name__ == "__main__":
-    main()
+    import sys
+    if len(sys.argv) < 3:
+        print("Usage: python book_ride.py <destination> <pickup_time>")
+    else:
+        dest = sys.argv[1]
+        time_str = sys.argv[2]
+        book_ride(dest, time_str)
