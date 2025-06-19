3013f9ba0084325c2c84e64bfde07d17d7b46312
Ziath <ziathadam@gmail.com>
2025-06-19 15:49:03 +0700

    init ryde

---
+++ ryde/cancel_ride.py
@@ -0,0 +1,15 @@
+import uiautomator2 as u2
+import time
+from utils import check_login_status, clear_unexpected_popups, accept_permissions, screen_components, find_components, find_components_by_id, coordinate_bounds
+def cancel_ride():
+    try:
+        d = u2.connect()
+        # sess = d.session("com.gojek.app") 
+        d.app_start("com.rydesharing.ryde", stop=False)
+
+    except Exception as e:
+        # d = u2.connect()
+        # sess = d.session("org.telegram.messenger") 
+        # notify_n8n("1333039921", e)
+        print(f"[Error] Failed to book ride: {e}")
+        return
